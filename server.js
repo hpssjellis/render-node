318eed827c4d912f316b6b35d313717f41719b7b
Jeremy Ellis <keyfreemusic@gmail.com>
2025-11-27 12:53:01 -0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
             <title>Browser Title</title>
         </head>
         <body>
-            <h3 align=center>T2A06-node-codesandbox</h3>
+            <h3 align=center>Render-node-backend</h3>
             <form action="/" method="post">
                 <label for="myText01">Enter Text:</label>
                 <input type="text" id="myText01" name="myText01">
